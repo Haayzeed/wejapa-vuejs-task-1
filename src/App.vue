f8dd1e4cd7452747dc68877ec756155307a9d80e
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-07-26 19:41:27 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <myName :name="name"></myName>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import myName from "./components/myName.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    myName
+  },
+  data() {
+    return {
+      name: "Dosunmu Afeez"
+    };
   }
 };
 </script>
@@ -19,8 +23,6 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/myName.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 45px;
+}
+</style>
