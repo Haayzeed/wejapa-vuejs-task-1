1062576e44df634ded7de3efb21b2f63a0db51cf
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-07-27 13:06:54 +0100

    initial commit

--- src/components/myName.vue
+++ src/components/myName.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{ name }}</h1>
+    <h1>Hi there, I am {{ name }}</h1>
   </div>
 </template>
 
@@ -20,6 +20,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
-  font-size: 45px;
+  font-size: 36px;
 }
 </style>
